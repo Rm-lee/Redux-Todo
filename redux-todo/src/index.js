6cfd9d3eb1be9f97ce7f901d0d7ce1b93ae74794
Roger Lee <rmlee0003@gmail.com>
2019-07-14 18:44:35 -0400

    added redex-dev tools required code/ wrapped app in Provider

--- redux-todo/src/index.js
+++ redux-todo/src/index.js
@@ -4,12 +4,17 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
+import reducer from './reducers/reducer'
 
-const store = createStore(todo);
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+<Provider>
+ <App />
+ </Provider>
+ , document.getElementById('root'));
 
